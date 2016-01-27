99224ea3fb5a7e95ac322fb8b6d6a3a604aa0926
wrightrc <wright.clay@gmail.com>
2016-01-26 21:51:15 -0800

    trying to fix polygonGate error

--- R/noSummaryAnalysis.R
+++ R/noSummaryAnalysis.R
@@ -137,63 +137,63 @@ steadyState <- function(flowset,ploidy="diploid", only="singlets"){
   }
 }
 
-#########################
-###  Cytometer Gates  ###
-#########################
-
-
-
-###These stricter gates were set for the NemLab cytometer "Special Snowflake" 7sept2015 by EPJ
-
-### EPJyeastGate
-### Defines an SSC.A vs FSC.A gate.  Includes only the yeast population from a flowSet
-
-yeastGate <<- polygonGate(filterId="Yeast",
-                             .gate=matrix(c(
-                               #xvalues
-                               4e5,50000,40000,1250000,2000000,
-                               #yvalues
-                               10000,15000,0.8e5,6e5,5e5),
-                               ncol=2,nrow=5,dimnames=list(c("1","1","1","1","1"),c("FSC.A","SSC.A"))))
-
-### Diploid Gates
-### Diploids are slightly larger and have better separation between singlets/doublets
-
-dipsingletGate <<- polygonGate(filterId="DipSingletGate",
-                                  .gate=matrix(c(
-                                    #x values
-                                    8e4,7e4,8e5,1.25e6,1e6,
-                                    #y values
-                                    8e4,1e5,1e6,1.25e6,1e6),
-                                    ncol=2,nrow=5,dimnames=list(rep(NA,5),c("FSC.A","FSC.H"))))
-
-dipdoubletGate <<- polygonGate(filterId="DipDoubletGate",
-                                  .gate=matrix(c(
-                                    #x values
-                                    4e5,4.5e4,2e5,1.3e6,2e6,2e6,
-                                    #y values
-                                    2.5e5,5e4,2e5,1.3e6,1.5e6,1e6),
-                                    ncol=2,nrow=6,dimnames=list(rep(NA,6),c("FSC.A","FSC.H"))))
-
-### Haploid Gates -->>still  need to set (next time I process haploid data)
-
-hapsingletGate <<- polygonGate(filterId="HaploidSingletGate",
-                                  .gate=matrix(c(
-                                    #x values
-                                    1e4,6.5e4,15e5,12e5,4e4,
-                                    #y values
-                                    4e4,8e4,16e5,18e5,4e5),
-                                    ncol=2,nrow=5,dimnames=list(rep(NA,5),c("FSC.A","FSC.H"))))
-
-hapdoubletGate <<- polygonGate(filterId="HaploidDoubletGate",
-                                  .gate=matrix(c(
-                                    #x values
-                                    1e4,2e5,30e5,25e5,25e5,6.75e5,
-                                    #y values
-                                    2e4,5e4,20e5,30e5,5e5,8.5e5),
-                                    ncol=2,nrow=6,dimnames=list(rep(NA,6),c("FSC.A","FSC.H"))))
-
-save(yeastGate, dipsingletGate, dipdoubletGate, hapsingletGate, hapdoubletGate, file = 'data/EPJGates.RData')
+# #########################
+# ###  Cytometer Gates  ###
+# #########################
+#
+#
+#
+# ###These stricter gates were set for the NemLab cytometer "Special Snowflake" 7sept2015 by EPJ
+#
+# ### EPJyeastGate
+# ### Defines an SSC.A vs FSC.A gate.  Includes only the yeast population from a flowSet
+#
+# yeastGate <<- polygonGate(filterId="Yeast",
+#                              .gate=matrix(c(
+#                                #xvalues
+#                                4e5,50000,40000,1250000,2000000,
+#                                #yvalues
+#                                10000,15000,0.8e5,6e5,5e5),
+#                                ncol=2,nrow=5,dimnames=list(c("1","1","1","1","1"),c("FSC.A","SSC.A"))))
+#
+# ### Diploid Gates
+# ### Diploids are slightly larger and have better separation between singlets/doublets
+#
+# dipsingletGate <<- polygonGate(filterId="DipSingletGate",
+#                                   .gate=matrix(c(
+#                                     #x values
+#                                     8e4,7e4,8e5,1.25e6,1e6,
+#                                     #y values
+#                                     8e4,1e5,1e6,1.25e6,1e6),
+#                                     ncol=2,nrow=5,dimnames=list(rep(NA,5),c("FSC.A","FSC.H"))))
+#
+# dipdoubletGate <<- polygonGate(filterId="DipDoubletGate",
+#                                   .gate=matrix(c(
+#                                     #x values
+#                                     4e5,4.5e4,2e5,1.3e6,2e6,2e6,
+#                                     #y values
+#                                     2.5e5,5e4,2e5,1.3e6,1.5e6,1e6),
+#                                     ncol=2,nrow=6,dimnames=list(rep(NA,6),c("FSC.A","FSC.H"))))
+#
+# ### Haploid Gates -->>still  need to set (next time I process haploid data)
+#
+# hapsingletGate <<- polygonGate(filterId="HaploidSingletGate",
+#                                   .gate=matrix(c(
+#                                     #x values
+#                                     1e4,6.5e4,15e5,12e5,4e4,
+#                                     #y values
+#                                     4e4,8e4,16e5,18e5,4e5),
+#                                     ncol=2,nrow=5,dimnames=list(rep(NA,5),c("FSC.A","FSC.H"))))
+#
+# hapdoubletGate <<- polygonGate(filterId="HaploidDoubletGate",
+#                                   .gate=matrix(c(
+#                                     #x values
+#                                     1e4,2e5,30e5,25e5,25e5,6.75e5,
+#                                     #y values
+#                                     2e4,5e4,20e5,30e5,5e5,8.5e5),
+#                                     ncol=2,nrow=6,dimnames=list(rep(NA,6),c("FSC.A","FSC.H"))))
+#
+# save(yeastGate, dipsingletGate, dipdoubletGate, hapsingletGate, hapdoubletGate, file = 'data/EPJGates.RData')
 
 # # Dynamic analysis --------------------------------------------------------
 # experiment<-"20150605_TIR1AFB2"
